1db09a2b048e560ef442fd43c917d2fbdd5d0506
Sanika Mane <msanika487@gmail.com>
2025-11-03 18:54:09 +0530

    Added Assignment 07_program3 file

---
+++ Assignments/Assignment 07_program3.cpp
@@ -0,0 +1,58 @@
+//PROGRAM//
+
+#include <iostream>
+using namespace std;
+
+class Vehicle {
+public:
+    Vehicle() {
+        cout << "This is a vehicle\n";
+    }
+};
+
+class Fare {
+public:
+    Fare() {
+        cout << "Fare of Vehicle\n";
+    }
+};
+
+class Car : public Vehicle {
+public:
+    Car() {
+        cout << "This vehicle is a car\n";
+    }
+};
+
+class Bus : public Vehicle, public Fare {
+public:
+    Bus() {
+        cout << "This vehicle is a Bus with fare\n";
+    }
+};
+
+int main() {
+    // ---- My Information ----
+    cout << "Name: Sanika Anil Mane\n";
+    cout << "Roll No: 96\n";
+    cout << "Division: B\n";
+    cout << "---------------------------------\n";
+
+    // Creating object of subclass will invoke base class constructors
+    Bus obj2;
+
+    return 0;
+}
+
+
+//OUTPUT//
+
+/*
+Name: Sanika Anil Mane
+Roll No: 96
+Division: B
+---------------------------------
+This is a vehicle
+Fare of Vehicle
+This vehicle is a Bus with fare
+*/
